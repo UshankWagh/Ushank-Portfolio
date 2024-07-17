cfc7b3164e4cf7a4a07c56bc8993bd3576b7f9e6
UshankWagh <ushankswagh@gmail.com>
2024-07-18 00:20:14 +0530

    enabled vercel analytics

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './components/Header';
 import Home from './pages/Home';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
     </Routes>
     <Header />
     <Home />
+    <Analytics />
   </Router>
 }
 
